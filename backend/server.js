dc827bb10d71eaca52515d65f213371ba5d9cbd7
AbdulBasiTcs1 <bearnwith@gmail.com>
2026-05-15 11:37:33 +0500

    setting up backend

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5001
+
+connectDB(process.env.DATABASE_URL)
+
+app.get("/", (req, res) => {
+    res.send("It's working");
+});
+
+app.listen(PORT, () => {
+    console.log("Server is up and running on port :",PORT);
+});
+
